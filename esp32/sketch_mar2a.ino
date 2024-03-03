77aa0a6e272b4a7eea71480bb907551b77c0b2fe
Yedla <14228270+Yedla009@users.noreply.github.com>
2024-03-02 22:40:55 -0500

    wifi connection setup is done

--- esp32/sketch_mar2a.ino
+++ esp32/sketch_mar2a.ino
@@ -1,28 +1,33 @@
-#include <dummy.h>
-
 #include <WiFi.h>
 
 const char* SSID = "ESP32-CAM Access Point";
-const char* PASSWORD = "123";
 
 void init_wifi(){
-  WiFi.softAP(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  Serial.print("WiFi turned on with SSID: ");
+  Serial.println(SSID);
+
+  WiFi.softAP(SSID);
+
+  Serial.println("Waiting: for client");
+
+  while (!(WiFi.softAPgetStationNum())){
+    delay(500); 
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  
+  Serial.println(".")
+  Serial.println("Client Connected");
 }
 
 void setup() {
+  // setting up ESP32 baud rate
+  Serial.begin(115200);
+
   // wait till a device is connected
   init_wifi();
-
-  // set up the
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
